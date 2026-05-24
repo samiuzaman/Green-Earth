4c0c6c7aaa5bf3aa703178bebae085b8e4e12894
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2026-05-24 20:46:49 +0600

    Display All Plants Card

--- js/script.js
+++ js/script.js
@@ -26,3 +26,48 @@ const displayAllCategory = (category) => {
     categoryListContainer.appendChild(li);
   });
 };
+
+// load all trees data
+const loadAllTrees = () => {
+  const url = "https://openapi.programming-hero.com/api/plants";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAllPlantsCard(data.plants));
+};
+loadAllTrees();
+
+const displayAllPlantsCard = (plants) => {
+  plants.forEach((plant) => {
+    const { id, image, name, description, category, price } = plant || {};
+    console.log(plant);
+    const plantCard = document.createElement("div");
+    plantCard.classList.add(
+      "w-full",
+      "bg-white",
+      "rounded-lg",
+      "p-4",
+      "shadow-lg",
+      "space-y-4",
+      "flex",
+      "flex-col",
+      "justify-between",
+      "items-center",
+    );
+    plantCard.innerHTML = `
+      <div class="w-full h-[165px]">
+        <img class="w-full h-full rounded-sm" src="${image}">
+      </div>
+
+      <div class="space-y-3">
+        <h4 class="text-darkColor text-lg font-bold">${name}</h4>
+        <p class="text-grayColor text-sm text-justify"> ${description} </p>
+        <div class="flex justify-between items-center py-1">
+          <span class="bg-lightGreenColor py-1 px-4 rounded-full text-primaryColor">${category}</span>
+          <span class="font-bold">৳${price}</span>
+        </div>
+        <button class="bg-primaryColor py-2 text-white w-full rounded-full cursor-pointer">Add to Cart</button>
+        </div>
+    `;
+    plantsCardContainer.appendChild(plantCard);
+  });
+};
